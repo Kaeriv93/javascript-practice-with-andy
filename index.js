604b83b6dc9648a5b93711e0c395cc7d1a9bf90d
kaeriv93 <vsan93@yahoo.com>
2022-08-08 22:59:39 -0500

    reviewing notes

---
+++ index.js
@@ -0,0 +1,18 @@
+// Variables
+// let, const, var
+// let and var are interchangable while const is a constant that cannot be changed
+
+//Example
+// let a = 10
+// console.log(`The initial value of a is: ${a}`)
+// a = 15
+// ^ we declared a variable named a which was originally assigned as 10
+// console.log(`Now the value of a is: ${a}`)
+
+//Functions
+// A function is a reusable block of a code
+function firstName(/*parameters are inserted here*/name){
+    return `My name is ${name}`
+}
+console.log(firstName('Andy'))
+console.log(firstName('Vireak'))
